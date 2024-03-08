558dc0882e6915eb8d43baa0647fbb6ee1f26f57
annelida <viqsiq@gmail.com>
2024-03-08 15:01:19 +0000

    Selenium webdriver practice

--- practice/selenium/ex_open_chrome.py
+++ practice/selenium/ex_open_chrome.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
+
 driver = webdriver.Chrome()
 driver.get("https://annelida.github.io/")
+elem = driver.find_element(By.ID, "text-table-of-contents").text
 time.sleep(1) # wait 1 second
+print(elem)
 driver.quit()
