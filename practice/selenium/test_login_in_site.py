310c35c4e168b2eee9a447c88c4b470076b6eedb
annelida <viqsiq@gmail.com>
2024-03-21 17:41:28 +0000

    Python testing with Selenium Webdriver practice

--- practice/selenium/test_login_in_site.py
+++ practice/selenium/test_login_in_site.py
@@ -7,19 +7,20 @@ import time
 class LoginScenario(unittest.TestCase):
 
     def setUp(self):
+
         self.driver = webdriver.Chrome()
+        self.driver.get("http://travel.agileway.net")
 
     def test_login_in_site(self):
 
-        driver = self.driver
-        driver.get("http://travel.agileway.net")
-        driver.find_element(By.ID, "username").send_keys("agileway") # Textfield
-        driver.find_element(By.ID, "password").send_keys("testwise") # Textfield
-        driver.find_element(By.NAME, "remember_me").click() # Checkbox
-        self.assertTrue(driver.find_element(By.CSS_SELECTOR, "input[value='Sign in']").is_displayed())
-        self.assertTrue(driver.find_element(By.CSS_SELECTOR, "input[value='Sign in']").is_enabled())
-        driver.find_element(By.CSS_SELECTOR, "input[value='Sign in']").click() #Button
+        self.driver.find_element(By.ID, "username").clear() # clear a text field
+        self.driver.find_element(By.ID, "username").send_keys("agileway") # Textfield
+        self.driver.find_element(By.ID, "password").send_keys("testwise") # Textfield
+        self.driver.find_element(By.NAME, "remember_me").click() # Checkbox
+        self.assertTrue(self.driver.find_element(By.CSS_SELECTOR, "input[value='Sign in']").is_enabled())
+        self.driver.find_element(By.CSS_SELECTOR, "input[value='Sign in']").click() #Button
         time.sleep(1) # wait 1 second
+
 
     def tearDown(self):
         self.driver.quit()
