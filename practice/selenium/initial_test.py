6bc497fe593dc3c236a71f0534a3b7a777fc8a65
annelida <viqsiq@gmail.com>
2024-03-26 09:43:08 +0000

    Accepting cookies in Google Selenium WebDriver

--- practice/selenium/initial_test.py
+++ practice/selenium/initial_test.py
@@ -4,10 +4,11 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
+driver.find_element(By.ID, "L2AGLb").click() 
 elem = driver.find_element(By.NAME, "q")
 elem.send_keys("Hello WebDriver!")
 elem.send_keys(Keys.RETURN)
-
 print(driver.title)
+driver.close()
 
 
