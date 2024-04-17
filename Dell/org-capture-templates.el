882826791bc1aca7784faa18930033071958b83c
annelida <viqsiq@gmail.com>
2024-04-17 10:41:04 +0100

    Org-capture extension

--- Dell/org-capture-templates.el
+++ Dell/org-capture-templates.el
@@ -8,6 +8,25 @@
 	 :immediate-finish 1
 	 :empty-lines-after 1)
 	;;
+        ("g" "Gathering");;<--prefix key for gathering text
+        ("gs" "SSH" item (file+headline
+			       "~/git/annelida/ballyboe/org-blog/2024-04-09-SSH-connection-to-remote-host-in-Linux.org" "Notes")
+         " %?%i "
+	 :immediate-finish 1
+         :empty-lines-after 1)
+        ;;
+        ("gp" "PM" item (file+headline
+			       "~/git/annelida/ballyboe/org-blog/2024-04-12-process-management-on-linux.org" "Notes")
+         " %?%i "
+	 :immediate-finish 1
+         :empty-lines-after 1)
+        ;;
+        ("gf" "FS" item (file+headline
+			       "~/git/annelida/ballyboe/org-blog/2024-04-12-linux-file-system-structure.org" "Notes")
+         " %?%i "
+	 :immediate-finish 1
+         :empty-lines-after 1)
+        ;;
         ))
 
 
