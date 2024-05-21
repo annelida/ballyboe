3d68844a7f987108073cca9c60bbd01a9cd3f04c
annelida <viqsiq@gmail.com>
2024-05-21 11:00:39 +0100

    Establishing route for 'components.html' page

--- practice/flask/app.py
+++ practice/flask/app.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     #return render_template("base.html", title="Jinja and Flask")
-    return render_template("index.html", title="Welcome to TwitterBootstrap")
+    return render_template('index.html', title='Welcome to TwitterBootstrap')
 
-
-@app.route("/scaffolding/")
+@app.route('/scaffolding')
 def scaffolding():
-    return render_template("scaffolding.html", title="Scaffolding")
+    return render_template('scaffolding.html', title='Scaffolding')
+
 @app.route('/base-css')
 def base():
-    return render_template('base-css.html', title="Base-Twitter Bootstrap")
+    return render_template('base-css.html', title='Base-Twitter Bootstrap')
+
+@app.route('/components')
+def components():
+    return render_template('components.html', title='Components')
 
 if __name__ == "__main__":
     app.run(debug=True)
