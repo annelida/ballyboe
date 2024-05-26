5df2734d6adadf0e4c8be4e3242b432cbed6dda6
annelida <viqsiq@gmail.com>
2024-05-26 12:20:45 +0100

    Rendering a Jinja template variable

--- practice/my_flask_app/app.py
+++ practice/my_flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='My Flask App')
 
 if __name__ == '__main__':
     app.run(debug=True)
