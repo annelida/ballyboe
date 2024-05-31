75603a5181445d4f027f8d78443f009f4c997b16
annelida <viqsiq@gmail.com>
2024-05-31 11:48:04 +0100

    Setting a variable in Flask route function indicating which link should be active

--- practice/flask/app.py
+++ practice/flask/app.py
@@ -5,35 +5,35 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #return render_template("base.html", title="Jinja and Flask")
-    return render_template('index.html', title='Welcome to TwitterBootstrap')
+    return render_template('index.html', title='Welcome to TwitterBootstrap', active_page='home')
 
 @app.route('/scaffolding')
 def scaffolding():
-    return render_template('scaffolding.html', title='Scaffolding')
+    return render_template('scaffolding.html', title='Scaffolding', active_page='scaffolding')
 
 @app.route('/base-css')
 def base():
-    return render_template('base-css.html', title='Base-Twitter Bootstrap')
+    return render_template('base-css.html', title='Base-Twitter Bootstrap', active_page='base')
 
 @app.route('/components')
 def components():
-    return render_template('components.html', title='Components')
+    return render_template('components.html', title='Components', active_page='components')
 
 @app.route('/javascript')
 def javascript():
-    return render_template('javascript.html', title='Javascript-Twitter Bootstrap')
+    return render_template('javascript.html', title='Javascript-Twitter Bootstrap', active_page='javascript')
 
 @app.route('/less')
 def less():
-    return render_template('less.html', title='Less-Twitter Bootstrap')
+    return render_template('less.html', title='Less-Twitter Bootstrap', active_page='less')
 
 @app.route('/download')
 def download():
-    return render_template('download.html', title='Download-Twitter Bootstrap')
+    return render_template('download.html', title='Download-Twitter Bootstrap', active_page='download')
 
 @app.route('/examples')
 def examples():
-    return render_template('examples.html', title='Examples-Twitter Bootstrap')
+    return render_template('examples.html', title='Examples-Twitter Bootstrap', active_page='examples')
 
 if __name__ == "__main__":
     app.run(debug=True)
