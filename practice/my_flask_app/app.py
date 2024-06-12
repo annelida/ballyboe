72fcfb039c12e10873059a67eaad4580934b6f2a
annelida <vikky@localhost.localdomain>
2024-06-12 09:09:14 +0100

    Example of passing a dictionary from Flask to Jinja

--- practice/my_flask_app/app.py
+++ practice/my_flask_app/app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title='My Flask App')
 
+max_score = 100
+test_name = "Python Challenge"
+students = [
+    {"name": "Sandrine",  "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+    {"name": "Fritz", "score": 40},
+    {"name": "Sirius", "score": 75},
+]
+
+@app.route('/results')
+def results():
+    context = {
+        "title": "Results",
+        "students": students,
+        "test_name": test_name,
+        "max_score": max_score,
+    }
+    return render_template('results.html', **context)
+
 if __name__ == '__main__':
     app.run(debug=True)
