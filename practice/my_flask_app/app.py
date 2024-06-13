b0224831661d448c78927b46be246d6a93cf7db3
annelida <viqsiq@gmail.com>
2024-06-13 16:32:37 +0100

    Set up database connection and creation a Flask route to that connection

--- practice/my_flask_app/app.py
+++ practice/my_flask_app/app.py
@@ -1,9 +1,19 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect("/home/vikky/Downloads/scraped.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def home():
+    conn = get_db_connection()
+    items = conn.execute('SELECT name, sku, manufacturer FROM products LIMIT 10').fetchall()
+    conn.close()
+    return render_template('index.html', title='My Flask App', items=items)
 
 
 if __name__ == '__main__':
