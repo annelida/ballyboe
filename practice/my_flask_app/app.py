5e0a09c2a4cec80293cf4e047ffc37454f72596f
annelida <vikky@localhost.localdomain>
2024-06-22 20:34:20 +0100

    Corrections Flask controller file

--- practice/my_flask_app/app.py
+++ practice/my_flask_app/app.py
@@ -1,20 +1,21 @@
-import sqlite3
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect("/home/vikky/Downloads/scraped.db")
-    conn.row_factory = sqlite3.Row
-    return conn
-
 @app.route('/')
-def home():
-    conn = get_db_connection()
-    items = conn.execute('SELECT name, sku, manufacturer FROM products LIMIT 10').fetchall()
-    conn.close()
-    return render_template('index.html', title='My Flask App', items=items)
+def display_items():
+    conn = sqlite3.connect('/home/vikky/Downloads/scraped.db')
+    cursor = conn.cursor()
 
+    cursor.execute("SELECT name, sku, manufacturer FROM products LIMIT 10")
+    results = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('index.html', title='My Flask app', items=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
