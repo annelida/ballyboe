8acd5323e43f85b1b6e179e7d4d42374b7f574b8
annelida <viqsiq@gmail.com>
2024-07-12 19:28:34 +0100

    Including logging

--- practice/form_flask/app.py
+++ practice/form_flask/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, request, render_template
+import logging
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(
+    filename='app.log',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+)
+
 @app.route('/')
 def home():
-     return render_template('index.html')
+    app.logger.debug('Debug message: Home page accessed')
+    app.logger.info('Info message: Home page accessed')
+    return render_template('index.html')
 
 @app.route('/greet', methods=['POST'])
 def greet():
     name = request.form.get('name')
+    app.logger.debug(f'Debug message: Greeting {name}')
+    app.logger.info(f'Info message: Greeting {name}')
     return f"Hello, {name}!"
 
 if __name__ == '__main__':
